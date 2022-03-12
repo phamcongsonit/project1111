ede911b12814b7070de67592dca4ca7a820f8332
Pham Cong Son <phamcongsonit@gmail.com>
2022-03-12 18:20:52 +0700

    C

--- api/post.js
+++ api/post.js
@@ -6,11 +6,46 @@ const router = express.Router();
  *
  * @return product list | empty.
  */
-router.get("/", async (req, res) => {
+
+var adapterFor = (function() {
+	var url = require('url'),
+	adapters = {
+		'http:': require('http'),
+		'https:': require('https'),
+	};
+	return function(inputUrl) {
+		return adapters[url.parse(inputUrl).protocol]
+	}
+}());
+
+ // khai bao link url
+var rootURL = "https://aubtu.biz/";
+
+
+function getData(postID){
+	return new Promise(function(resolve, reject){
+		var url = rootURL + '?pcs=1&p=' + postID;
+		adapterFor(url).get(url, function(res){
+		    var body = '';
+		    res.on('data', function(chunk){
+		        body += chunk;
+		    });
+
+		    res.on('end', function(){
+		        resolve(body);
+		    });
+		}).on('error', function(e){
+			reject(e);
+		});
+
+	})
+}
+
+router.get("/:postID",  (req, res) => {
   try {
     res.json({
       status: 200,
-      message: "Get data has successfully",
+      message: "Get data has successfully: " + req.params.postID,
     });
   } catch (error) {
     console.error(error);
