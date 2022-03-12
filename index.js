4dd0ce76f13744b9b3790ba5e3de56742a07dc34
Pham Cong Son <phamcongsonit@gmail.com>
2022-03-12 18:12:24 +0700

    C

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
 const post = require("./api/post");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
 app.use("/post", post);
 
 const PORT = process.env.PORT || 8080;
