a450b4fd68ede60a4e08c7bc8acbf9aef4c66e45
Pham Cong Son <phamcongsonit@gmail.com>
2022-03-12 18:08:59 +0700

    C

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
+const post = require("./api/post");
 
 app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
+app.use("/post", post);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
