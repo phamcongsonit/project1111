bb4dc1ef7498061009d60dbd0f5f65473f47c999
Pham Cong Son <phamcongsonit@gmail.com>
2022-03-12 18:41:05 +0700

    C

--- api/post.js
+++ api/post.js
@@ -88,9 +88,56 @@ router.get("/:postID",  (req, res) => {
 				message: "Get data has successfully: " + req.params.postID,
 		});
 	}
-	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-	res.write("Chưa có ID post");
-	res.end();
+	if (showPost){
+		getData(postID).then(function(data){
+			res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+			// res.write("Referer: " + req.header('Referer') + "\n User Agent: " + req.get('User-Agent'));
+			var title = JSON.parse(data)['title'];
+			var content = JSON.parse(data)['content'];
+			var thumbnail = "https://" + req.hostname + "/uploads/" + JSON.parse(data)['id'] + ".jpg";
+			var description = JSON.parse(data)['description'];
+			var htmlCode = `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>` + title +`</title>
+				<meta property="og:locale" content="en_US">
+				<meta property="og:type" content="article">
+				<meta property="og:title" content="` + title +`">
+				<meta property="og:description" content="` + description +`">
+				<meta property="og:url" content="` + req.protocol + '://' + req.get('host') + req.originalUrl +`">
+				<meta property="og:site_name" content="We Love Animals">
+				<meta property="article:section" content="Animal">
+				<meta property="og:image" content="` + thumbnail + `">
+				<meta property="og:image:alt" content="` + title + `">
+       
+       <style>
+          img { width: 100%; height: auto; }
+          ul { list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: #333; }
+				  li {float: left; }
+				  li a { display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none; }
+				  li a:hover:not(.active) { background-color: #111; }
+				  .active { background-color: #4CAF50; }
+       </style>
+			</head>
+			<body style="background:#eee;"><div style="padding:20px;margin:30px auto;padding:20px;max-width:800px;background:white;box-shadow: 5px 5px 5px #888888;">
+      <div>
+				<ul>
+					<li><a href="#home">Home</a></li>
+					<li><a href="#news">News</a></li>
+					<li><a href="#contact">Contact</a></li>
+					<li style="float:right"><a class="active" href="#about">About</a></li>
+				</ul>
+			</div>
+        <h1>` + title + `</h1>
+        <img style="width:100%;height:auto;" src="`  + thumbnail + `">` + content + `</div>
+      </body>
+			</html>`
+			res.write(htmlCode);
+			res.end();
+		})
+	}
 
 });
 
